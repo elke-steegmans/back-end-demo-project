f847f72a65cb9c3f92e66e7bf806eb95c9fb2cc0
elke-steegmans <Elke.Steegmans@ucll.be>
2024-09-26 14:16:15 +0200

    Initial commit

---
+++ src/main/java/be/ucll/controller/UserRestController.java
@@ -0,0 +1,60 @@
+package be.ucll.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+
+import be.ucll.model.DomainException;
+import be.ucll.model.User;
+import be.ucll.service.ServiceException;
+import be.ucll.service.UserService;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/users")
+@CrossOrigin(origins = "http://localhost:8000")
+public class UserRestController {
+
+    private UserService userService;
+
+    public UserRestController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PostMapping()
+    public User addUser(@Valid @RequestBody User user) {
+        return userService.addUser(user);
+    }
+
+    @ExceptionHandler(DomainException.class)
+    public ResponseEntity<Map<String, String>> handlDomainException(DomainException ex, WebRequest request) {
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("DomainException: ", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(errorResponse);
+    }
+
+    @ExceptionHandler(ServiceException.class)
+    public ResponseEntity<Map<String, String>> handlServiceException(ServiceException ex, WebRequest request) {
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("ServiceException: ", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(errorResponse);
+    }
+}
